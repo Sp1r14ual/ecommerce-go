aacdcf5cd9b3818040583fd45e7abb11bae71e8f
Sp1r14ual <sp1r14ual@gmail.com>
2026-07-30 16:48:54 +0700

    add supplementary comments to user model in auth-service

--- auth-service/internal/domain/user.go
+++ auth-service/internal/domain/user.go
@@ -1,7 +1,8 @@
 package domain
 
+// User описывает сущность пользователя в нашей системе
 type User struct {
 	ID           int64
 	Email        string
-	PasswordHash string
+	PasswordHash string // Мы никогда не храним пароль в открытом виде!
 }
